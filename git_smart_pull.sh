6242d3fc2926fd3237487d9810df70b260c34331
Jerry <superzcj_001@163.com>
2019-05-30 10:12:35 +0800

    Update 修改脚本版本号 🔨

--- git_smart_pull.sh
+++ git_smart_pull.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh
 # @author  Jerry <superzcj_001@163.com>
-# @version 0.1.0
+# @version 0.1.1
 
 echo -e ' \033[36m@ \033[34m欢迎使用智能拉代码脚本\033[0m'
 echo -e ' \033[36m@ \033[32m作者 \033[35mJerry\033[0m'
-echo -e ' \033[36m@ \033[32m版本 \033[35m0.1.0\033[0m'
+echo -e ' \033[36m@ \033[32m版本 \033[35m0.1.1\033[0m'
 
 REMOTE=$(git remote)
 BRANCH=$(git symbolic-ref --short -q HEAD)
