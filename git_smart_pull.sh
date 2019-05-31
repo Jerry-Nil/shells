d0ee55e05b9ed3199f74642a27224cb12495714a
Jerry <superzcj_001@163.com>
2019-05-31 16:10:40 +0800

    Update 现在脚本在判断工作目录状态时仅判断是否有文件新增或修改，未加入版本控制的文件不计数 🔨

--- git_smart_pull.sh
+++ git_smart_pull.sh
@@ -1,14 +1,14 @@
 #!/bin/zsh
 # @author  Jerry <superzcj_001@163.com>
-# @version 0.1.1
+# @version 0.1.2
 
 echo -e ' \033[36m@ \033[34m欢迎使用智能拉代码脚本\033[0m'
 echo -e ' \033[36m@ \033[32m作者 \033[35mJerry\033[0m'
-echo -e ' \033[36m@ \033[32m版本 \033[35m0.1.1\033[0m'
+echo -e ' \033[36m@ \033[32m版本 \033[35m0.1.2\033[0m'
 
 REMOTE=$(git remote)
 BRANCH=$(git symbolic-ref --short -q HEAD)
-STATUS=$(git status --porcelain 2> /dev/null | tail -n1)
+STATUS=$(git status --porcelain 2> /dev/null | grep -E '[MA]')
 
 echo
 echo -e " \033[36m@ 本地: \033[32m"$BRANCH"\033[0m"
