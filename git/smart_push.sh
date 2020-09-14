6abfd9defa33ba6e2e1a9d6e26a2b9784aed2e01
Jerry <superzcj_001@163.com>
2020-09-14 13:38:56 +0800

    Update push脚本现在推送前会检查远程仓库是否存在对应分支 🔨

--- git/smart_push.sh
+++ git/smart_push.sh
@@ -1,10 +1,13 @@
 #!/bin/zsh
 # @author  Jerry <superzcj_001@163.com>
-# @version 0.1.5
+# @version 0.1.6
+
+AUTHOR='Jerry'
+VERSION='0.1.6'
 
 echo -e ' \033[36m@ \033[34m欢迎使用智能推代码脚本\033[0m'
-echo -e ' \033[36m@ \033[32m作者 \033[35mJerry\033[0m'
-echo -e ' \033[36m@ \033[32m版本 \033[35m0.1.5\033[0m'
+echo -e " \033[36m@ \033[32m作者 \033[35m${AUTHOR}\033[0m"
+echo -e " \033[36m@ \033[32m版本 \033[35m${VERSION}\033[0m"
 
 REMOTE=$(git remote)
 BRANCH=$(git symbolic-ref --short -q HEAD)
@@ -23,35 +26,46 @@ if [[ -n $STATUS ]]; then
 	echo -e ' \033[36m@ 已贮藏\033[0m'
 fi
 
-echo
-echo -e ' \033[36m@ 准备获取远程仓库最新改动\033[0m'
-echo
-
-git fetch $REMOTE $BRANCH --prune
-
-echo
-echo -e ' \033[36m@ 获取完毕\033[0m'
-echo
-echo -e ' \033[36m@ 与远程分支比较差异...\033[0m'
-DIFF=$(git log $BRANCH..$REMOTE/$BRANCH --oneline 2> /dev/null | tail -n1)
-
-if [[ -n $DIFF ]]; then
-	echo -e ' \033[36m@ 存在差异, 准备变基式拉取...\033[0m'
+remote_branch_count=$(git ls-remote --heads 2>/dev/null | grep "heads/feature/${BRANCH}$" | wc -l)
+if [[ $remote_branch_count -gt 0 ]]; then
 	echo
-	git pull $REMOTE $BRANCH --rebase
+	echo -e ' \033[36m@ 准备获取远程仓库最新改动\033[0m'
 	echo
-	echo -e ' \033[36m@ 拉取完毕\033[0m'
+
+	git fetch $REMOTE $BRANCH --prune
+
+	echo
+	echo -e ' \033[36m@ 获取完毕\033[0m'
+	echo
+	echo -e ' \033[36m@ 与远程分支比较差异...\033[0m'
+	DIFF=$(git log $BRANCH..$REMOTE/$BRANCH --oneline 2> /dev/null | tail -n1)
+
+	if [[ -n $DIFF ]]; then
+		echo -e ' \033[36m@ 存在差异, 准备变基式拉取...\033[0m'
+		echo
+		git pull $REMOTE $BRANCH --rebase
+		echo
+		echo -e ' \033[36m@ 拉取完毕\033[0m'
+	else
+		echo
+		echo -e ' \033[36m@ 无差异，跳过拉取代码\033[0m'
+	fi
+
+	echo
+	echo -e ' \033[36m@ 准备推送\033[0m'
+	echo
+	git push $REMOTE $BRANCH
+	echo
+	echo -e ' \033[36m@ 推送完成\033[0m'
 else
 	echo
-	echo -e ' \033[36m@ 无差异，跳过拉取代码\033[0m'
+	echo -e ' \033[36m@ 准备推送\033[0m'
+	echo
+	git push --set-upstream $REMOTE $BRANCH
+	echo
+	echo -e ' \033[36m@ 推送完成\033[0m'
 fi
 
-echo
-echo -e ' \033[36m@ 准备推送\033[0m'
-echo
-git push $REMOTE $BRANCH
-echo
-echo -e ' \033[36m@ 推送完成\033[0m'
 
 if [[ -n $STATUS ]]; then
 	echo
